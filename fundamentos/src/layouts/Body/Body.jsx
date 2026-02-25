42183a00b917d510017e864f3e8f639087b00c45
Luciano Rocha <luciano_rp7@hotmail.com>
2026-02-25 10:03:09 -0300

    feat: fundamentos do react

---
+++ fundamentos/src/layouts/Body/Body.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import Card from '../../components/Card/Card'
+
+import styles from './Body.module.css'
+
+const Body = () => {
+  //array com nomes diferentes
+
+  const usuarios = [
+    {nome:"Ana", idade: 22, cidade:"São José"},
+    {nome:"Bruno", idade: 30, cidade:"Florianópolis"},
+    {nome:"João", idade: 35, cidade:"Palhoça"},
+    {nome:"Ana", idade: 22, cidade:"São José"},
+    {nome:"Bruno", idade: 30, cidade:"Florianópolis"},
+    {nome:"João", idade: 35, cidade:"Palhoça"},
+    {nome:"Ana", idade: 22, cidade:"São José"},
+    {nome:"Bruno", idade: 30, cidade:"Florianópolis"},
+    {nome:"João", idade: 35, cidade:"Palhoça"},
+  ]
+
+  return (
+    <>
+       <main className={styles.body}>
+            <h2>Usuários Cadastrados</h2>
+            <div className={styles.cardContainer}>
+                {usuarios.map((usuario, index) => (
+                    <Card
+                        key={index}
+                        nome={usuario.nome}
+                        idade={usuario.idade}
+                        cidade={usuario.cidade}
+                    />
+                ))}
+            </div>
+       </main>
+    </>
+  )
+}
+
+export default Body
